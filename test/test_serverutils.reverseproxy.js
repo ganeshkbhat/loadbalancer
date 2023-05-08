ab2940bf8c4e3fdc20bd988c3915a9ac4f23bc9d
Ganesh K Bhat <ganeshsurfs@gmail.com>
2023-05-09 01:41:10 +0530

    [COMMIT] Minor changes to serverProxy reverseProxy functions in serverutils. Adding name change of createProxy to createNetProxy. Making minor changes to reverseProxy for bugs. Adding Minor bug fixes for server function in serverutils

--- test/test_serverutils.reverseproxy.js
+++ test/test_serverutils.reverseproxy.js
@@ -1,7 +1,7 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
 const http = require('http');
-const createReverseProxy = require('../index').serverutils;
+const createReverseProxy = require('../index').serverutils.createReverseProxy;
 
 describe('reverse proxy', () => {
     let proxyServer, targetServer, requestSpy;
