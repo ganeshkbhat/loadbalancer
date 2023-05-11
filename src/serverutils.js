162352060f7f5b10b3a29e6505226a5502e811bc
Ganesh K Bhat <ganeshsurfs@gmail.com>
2023-05-12 00:16:55 +0530

    [COMMIT] Adding functions for sockets. Base functions for http-https, websocket, and socket have been moved to sockets file. Minor bug fixes and formating for http/s request client functions. Adding socket connection. Adding serverutils function modifications. Adding minor changes to sockets and serverutils functions. Adding tests file for tests for mocha and sinon. Adding minor changes to threading function files. Changing keys key word into certs.

--- src/serverutils.js
+++ src/serverutils.js
@@ -135,86 +135,19 @@ function reverseProxy(serverOptions) {
 }
 
 
-// /**
-//  *
-//  *
-//  * @param {*} serverOptions
-//  * 
-//  * Credit: https://stackoverflow.com/a/32104777/3204942
-//  * 
-//  */
-// function createNetProxy(serverOptions) {
-//     const net = require('net');
-//     const url = require('url');
-//     hostname = serverOptions?.host;
-//     port = serverOptions?.port; // || process.env.PORT 
-
-
-//     let server;
-
-
-//     const listener = server.listen(port, hostname, (err) => {
-//         if (err) return console.error(err);
-//         const info = listener.address();
-//         console.log(`Server is listening on address ${info.address} port ${info.port}`)
-//     });
-
-//     server.on('connect', (req, clientSocket, head) => { // listen only for HTTP/1.1 CONNECT method
-//         console.log(clientSocket.remoteAddress, clientSocket.remotePort, req.method, req.url)
-//         if (!req.headers['proxy-authorization']) { // here you can add check for any username/password, I just check that this header must exist!
-//             clientSocket.write([
-//                 'HTTP/1.1 407 Proxy Authentication Required',
-//                 'Proxy-Authenticate: Basic realm="proxy"',
-//                 'Proxy-Connection: close',
-//             ].join('\r\n'))
-//             clientSocket.end('\r\n\r\n')  // empty body
-//             return
-//         }
-//         const { port, hostname } = url.parse(`//${req.url}`, false, true) // extract destination host and port from CONNECT request
-//         if (hostname && port) {
-//             const serverErrorHandler = (err) => {
-//                 console.error(err.message)
-//                 if (clientSocket) {
-//                     clientSocket.end(`HTTP/1.1 500 ${err.message}\r\n`)
-//                 }
-//             }
-//             const serverEndHandler = () => {
-//                 if (clientSocket) {
-//                     clientSocket.end(`HTTP/1.1 500 External Server End\r\n`)
-//                 }
-//             }
-//             const serverSocket = net.connect(port, hostname) // connect to destination host and port
-//             const clientErrorHandler = (err) => {
-//                 console.error(err.message)
-//                 if (serverSocket) {
-//                     serverSocket.end()
-//                 }
-//             }
-//             const clientEndHandler = () => {
-//                 if (serverSocket) {
-//                     serverSocket.end()
-//                 }
-//             }
-//             clientSocket.on('error', clientErrorHandler)
-//             clientSocket.on('end', clientEndHandler)
-//             serverSocket.on('error', serverErrorHandler)
-//             serverSocket.on('end', serverEndHandler)
-//             serverSocket.on('connect', () => {
-//                 clientSocket.write([
-//                     'HTTP/1.1 200 Connection Established',
-//                     'Proxy-agent: Node-VPN',
-//                 ].join('\r\n'))
-//                 clientSocket.write('\r\n\r\n') // empty body
-//                 // "blindly" (for performance) pipe client socket and destination socket between each other
-//                 serverSocket.pipe(clientSocket, { end: false })
-//                 clientSocket.pipe(serverSocket, { end: false })
-//             })
-//         } else {
-//             clientSocket.end('HTTP/1.1 400 Bad Request\r\n')
-//             clientSocket.destroy()
-//         }
-//     });
-// }
+/**
+ *
+ *
+ * @param {*} socketOptions
+ * 
+ * Credit: https://stackoverflow.com/a/32104777/3204942
+ * 
+ */
+function createNetProxy(socketOptions) {
+    const net = require('net');
+    const url = require('url');
+    
+}
 
 
 /**
