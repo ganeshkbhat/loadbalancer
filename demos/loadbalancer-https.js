2ade1f61ea35e5b5c17d7f8d43b4906fe85d7f47
Ganesh K Bhat <ganeshsurfs@gmail.com>
2023-05-11 18:51:23 +0530

    [COMMIT] Adding functions for sockets. Base functions for http-https, websocket, and socket have been moved to sockets file. Minor bug fixes and formating for http/s request client functions. Adding socket connection. Adding serverutils function modifications. Adding minor changes to sockets and serverutils functions. Adding tests file for tests for mocha and sinon. Adding minor changes to threading function files

--- demos/loadbalancer-https.js
+++ demos/loadbalancer-https.js
@@ -6,7 +6,7 @@
  * Install: npm i loadbalancer --save
  * Github: https://github.com/ganeshkbhat/loadbalancer
  * npmjs Link: https://www.npmjs.com/package/loadbalancer
- * File: demos/loadbalancer-server.js
+ * File: demos/loadbalancer-https.js
  * File Description: A simple threaded and clustered load balancer for nodejs
  * 
  * 
